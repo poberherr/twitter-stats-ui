8ccbc336e1a92a5644015ab7921692ec7fe2ee8e
Patrick Oberherr <patrick.oberherr@gmail.com>
2016-02-25 13:18:41 +0100

    Ading new metrics to dashboard

--- index.js
+++ index.js
@@ -27,12 +27,8 @@ const chartTweetsByDay = c3.generate({
 const chartDevices = c3.generate({
   bindto: '#chartDevices',
     data: {
-        // iris data from R
         columns: [],
         type : 'pie',
-        onclick: function (d, i) { console.log("onclick", d, i); },
-        onmouseover: function (d, i) { console.log("onmouseover", d, i); },
-        onmouseout: function (d, i) { console.log("onmouseout", d, i); }
     }
 });
 
@@ -41,9 +37,6 @@ const chartOwnTweets = c3.generate({
     data: {
         columns: [],
         type: 'gauge',
-        onclick: function (d, i) { console.log("onclick", d, i); },
-        onmouseover: function (d, i) { console.log("onmouseover", d, i); },
-        onmouseout: function (d, i) { console.log("onmouseout", d, i); }
     },
     gauge: {},
     color: {
@@ -62,15 +55,19 @@ const chartMostUsedWords = c3.generate({
     data: {
         columns: [],
         type : 'donut',
-        onclick: function (d, i) { console.log("onclick", d, i); },
-        onmouseover: function (d, i) { console.log("onmouseover", d, i); },
-        onmouseout: function (d, i) { console.log("onmouseout", d, i); }
     },
     donut: {
         title: "Words matter!"
     }
 });
 
+const chartTopReplyUsers = c3.generate({
+  bindto: '#chartTopReplyUsers',
+    data: {
+        columns: [],
+        type : 'pie',
+    }
+});
 
 document.querySelector('#search').addEventListener('keypress', function (e) {
     var key = e.which || e.keyCode;
@@ -85,7 +82,7 @@ document.querySelector('#search').addEventListener('keypress', function (e) {
 
 
 
-fetchUrl('http://localhost:5000/statistics/nellykfm').then((json) => {
+fetchUrl('http://localhost:5000/statistics/rlsux').then((json) => {
   const sourceDevice = json.data.attributes.user_devices.map((device) => {
     return [device.source, device.count];
   });
@@ -94,7 +91,13 @@ fetchUrl('http://localhost:5000/statistics/nellykfm').then((json) => {
   const mostUsedWords = json.data.attributes.most_words_used.map((mostWords) => {
     return [mostWords.source, mostWords.count];
   });
+
+  const topReplyUsers = json.data.attributes.top_reply_users.map((topUsers) => {
+    return [topUsers.source, topUsers.count];
+  });
+
   const daysOfTweets = json.data.attributes.stats_per_day;
+
 
   document.getElementById('UserScreenName1').innerHTML = 'Overview for ' + json.user.data.attributes.screen_name;
   document.getElementById('UserScreenName2').innerHTML = json.user.data.attributes.screen_name;
@@ -104,13 +107,17 @@ fetchUrl('http://localhost:5000/statistics/nellykfm').then((json) => {
   document.getElementById('UserTwitterId').innerHTML = 'Twitter ID: ' + json.user.data.attributes.twitter_id;
   document.getElementById('UserLocation').innerHTML = 'Location: ' + json.user.data.attributes.location;
   document.getElementById('UserTimeZone').innerHTML = 'Time Zone: ' + json.user.data.attributes.time_zone;
-  document.getElementById('UserCreatedAt').innerHTML = 'Account created: ' + json.user.data.attributes.created_at;
+  document.getElementById('UserCreatedAt').innerHTML = 'Account created:<br>' + json.user.data.attributes.created_at;
 
   document.getElementById('UserTweetsCount').innerHTML = json.user.data.attributes.statuses_count + ' tweets';
   document.getElementById('UserTweetsAnalyzed').innerHTML = json.data.attributes.tweets_analysed + ' Tweets analysed';
-  document.getElementById('UserFavoritesCount').innerHTML = json.user.data.attributes.favourites_count + ' likes in total';
+  document.getElementById('UserFavoritesCount').innerHTML = json.user.data.attributes.favourites_count + ' total likes collected';
   document.getElementById('UserFollowersCount').innerHTML = json.user.data.attributes.followers_count + ' followers';
 
+  document.getElementById('UserGotRetweetedCount').innerHTML = json.data.attributes.user_got_retweeted_count + ' retweets collected';
+  document.getElementById('UserAverageOwnTweetsVsRetweetCount').innerHTML = json.data.attributes.avg_own_tweets_vs_retweeted_count + ' of your tweets get retweeted';
+  document.getElementById('UserAverageStars').innerHTML = json.data.attributes.average_stars + ' avg stars per tweet';
+  document.getElementById('UserAverageRetweets').innerHTML = json.data.attributes.average_retweets + ' avg retweets';
 
   chartDevices.unload();
   chartDevices.load({
@@ -130,6 +137,11 @@ fetchUrl('http://localhost:5000/statistics/nellykfm').then((json) => {
   chartTweetsByDay.unload();
   chartTweetsByDay.load({
     columns: daysOfTweets
+  });
+
+  chartTopReplyUsers.unload();
+  chartTopReplyUsers.load({
+    columns: topReplyUsers
   });
 
 });
--- js/test.js
+++ js/test.js
@@ -76,12 +76,8 @@
 	const chartDevices = c3.generate({
 	  bindto: '#chartDevices',
 	    data: {
-	        // iris data from R
 	        columns: [],
 	        type : 'pie',
-	        onclick: function (d, i) { console.log("onclick", d, i); },
-	        onmouseover: function (d, i) { console.log("onmouseover", d, i); },
-	        onmouseout: function (d, i) { console.log("onmouseout", d, i); }
 	    }
 	});
 	
@@ -90,9 +86,6 @@
 	    data: {
 	        columns: [],
 	        type: 'gauge',
-	        onclick: function (d, i) { console.log("onclick", d, i); },
-	        onmouseover: function (d, i) { console.log("onmouseover", d, i); },
-	        onmouseout: function (d, i) { console.log("onmouseout", d, i); }
 	    },
 	    gauge: {},
 	    color: {
@@ -111,15 +104,19 @@
 	    data: {
 	        columns: [],
 	        type : 'donut',
-	        onclick: function (d, i) { console.log("onclick", d, i); },
-	        onmouseover: function (d, i) { console.log("onmouseover", d, i); },
-	        onmouseout: function (d, i) { console.log("onmouseout", d, i); }
 	    },
 	    donut: {
 	        title: "Words matter!"
 	    }
 	});
 	
+	const chartTopReplyUsers = c3.generate({
+	  bindto: '#chartTopReplyUsers',
+	    data: {
+	        columns: [],
+	        type : 'pie',
+	    }
+	});
 	
 	document.querySelector('#search').addEventListener('keypress', function (e) {
 	    var key = e.which || e.keyCode;
@@ -134,7 +131,7 @@
 	
 	
 	
-	fetchUrl('http://localhost:5000/statistics/nellykfm').then((json) => {
+	fetchUrl('http://localhost:5000/statistics/rlsux').then((json) => {
 	  const sourceDevice = json.data.attributes.user_devices.map((device) => {
 	    return [device.source, device.count];
 	  });
@@ -143,7 +140,13 @@
 	  const mostUsedWords = json.data.attributes.most_words_used.map((mostWords) => {
 	    return [mostWords.source, mostWords.count];
 	  });
+	
+	  const topReplyUsers = json.data.attributes.top_reply_users.map((topUsers) => {
+	    return [topUsers.source, topUsers.count];
+	  });
+	
 	  const daysOfTweets = json.data.attributes.stats_per_day;
+	
 	
 	  document.getElementById('UserScreenName1').innerHTML = 'Overview for ' + json.user.data.attributes.screen_name;
 	  document.getElementById('UserScreenName2').innerHTML = json.user.data.attributes.screen_name;
@@ -153,13 +156,17 @@
 	  document.getElementById('UserTwitterId').innerHTML = 'Twitter ID: ' + json.user.data.attributes.twitter_id;
 	  document.getElementById('UserLocation').innerHTML = 'Location: ' + json.user.data.attributes.location;
 	  document.getElementById('UserTimeZone').innerHTML = 'Time Zone: ' + json.user.data.attributes.time_zone;
-	  document.getElementById('UserCreatedAt').innerHTML = 'Account created: ' + json.user.data.attributes.created_at;
+	  document.getElementById('UserCreatedAt').innerHTML = 'Account created:<br>' + json.user.data.attributes.created_at;
 	
 	  document.getElementById('UserTweetsCount').innerHTML = json.user.data.attributes.statuses_count + ' tweets';
 	  document.getElementById('UserTweetsAnalyzed').innerHTML = json.data.attributes.tweets_analysed + ' Tweets analysed';
-	  document.getElementById('UserFavoritesCount').innerHTML = json.user.data.attributes.favourites_count + ' likes in total';
+	  document.getElementById('UserFavoritesCount').innerHTML = json.user.data.attributes.favourites_count + ' total likes collected';
 	  document.getElementById('UserFollowersCount').innerHTML = json.user.data.attributes.followers_count + ' followers';
 	
+	  document.getElementById('UserGotRetweetedCount').innerHTML = json.data.attributes.user_got_retweeted_count + ' retweets collected';
+	  document.getElementById('UserAverageOwnTweetsVsRetweetCount').innerHTML = json.data.attributes.avg_own_tweets_vs_retweeted_count + ' of your tweets get retweeted';
+	  document.getElementById('UserAverageStars').innerHTML = json.data.attributes.average_stars + ' avg stars per tweet';
+	  document.getElementById('UserAverageRetweets').innerHTML = json.data.attributes.average_retweets + ' avg retweets';
 	
 	  chartDevices.unload();
 	  chartDevices.load({
@@ -179,6 +186,11 @@
 	  chartTweetsByDay.unload();
 	  chartTweetsByDay.load({
 	    columns: daysOfTweets
+	  });
+	
+	  chartTopReplyUsers.unload();
+	  chartTopReplyUsers.load({
+	    columns: topReplyUsers
 	  });
 	
 	});
